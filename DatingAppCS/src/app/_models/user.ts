a5da30270d252ca2f60bd182b208ea74448d7959
Kroot <cpolesciuc99@gmail.com>
2020-07-20 12:47:05 +0300

    module 9

---
+++ DatingAppCS/src/app/_models/user.ts
@@ -0,0 +1,18 @@
+import { Photo } from './photo';
+
+export interface User {
+  id: number;
+  username: string;
+  knownAs: string;
+  age: number;
+  gender: string;
+  created: Date;
+  lastOnline: Date;
+  photoUrl: string;
+  city: string;
+  country: string;
+  interests?: string;
+  instruction: string;
+  lookingFor?: string;
+  photos?: Photo[];
+}
